1089fce37138ebcb06e4e205c55a92c04c810848
Chris Wacek <cwacek@gmail.com>
2012-08-20 14:04:36 -0400

    Added hoptable and pathtable userspace tools.

---
+++ tools/net-modeler-ctrl/hoptable.py
@@ -0,0 +1,57 @@
+import sys
+import struct
+import os
+
+HOPTABLE_PROC="/proc/net-modeler/hoptable"
+
+def read(args):
+  if not os.path.exists(HOPTABLE_PROC):
+    print "Cannot open proc interface: '{0}' does not appear to exist".format(HOPTABLE_PROC)
+    sys.exit(1)
+
+  try:
+    with open(HOPTABLE_PROC) as procf:
+      data = procf.readlines()
+  except IOError,e:
+    if e.errno == 13:
+      print "Don't have permissions to read '{0}'".format(HOPTABLE_PROC)
+      sys.exit(1)
+
+  for line in data:
+    print line.strip('\n')
+
+def __check_zero(name,arg,fmt=sys.stderr):
+  if arg < 0:
+    fmt.write("Error: '{0}' must be at least zero. Was '{1}'".format(name,arg))
+    return -1
+     
+  return 0
+
+def set_hops(args):
+
+  # hopid, bw, latency
+  hop_proc_fmt = "III"
+  if __check_zero('bw',args.bw):
+    sys.exit(-1)
+  if __check_zero('latency',args.latency):
+    sys.exit(-1)
+  if __check_zero('id',args.hopid):
+    sys.exit(-1)
+
+  bytestr = struct.pack(hop_proc_fmt,args.hopid,args.bw,args.latency)
+
+  if not os.path.exists(HOPTABLE_PROC):
+    print "Cannot open proc interface: '{0}' does not appear to exist".format(HOPTABLE_PROC)
+    sys.exit(1)
+
+  try:
+    with open(HOPTABLE_PROC,'w') as procf:
+      procf.write(bytestr);
+  except IOError,e:
+    if e.errno == 13:
+      print "Don't have permissions to write '{0}'".format(HOPTABLE_PROC)
+      sys.exit(1)
+    else:
+      print "Failed. Check 'dmesg' for more details."
+
+
--- tools/net-modeler-ctrl/net_modeler.py
+++ tools/net-modeler-ctrl/net_modeler.py
@@ -2,6 +2,8 @@ import sys
 import argparse
 import os
 import modelinfo
+import hoptable
+import pathtable
 
 def modelinfo_args(main_subparser):
   modelinfop = main_subparser.add_parser('modelinfo')
@@ -22,6 +24,40 @@ def modelinfo_args(main_subparser):
   modelinfo_set.add_argument('endpoints',help="The total number of endpoints in the model",
                                       type=int)
 
+def pathtable_args(main_subparser):
+  pathtablep = main_subparser.add_parser('pathtable')
+  pathtable_sub = pathtablep.add_subparsers()
+
+  pathtable_read = pathtable_sub.add_parser('read')
+  pathtable_read.set_defaults(func=pathtable.read)
+
+  pathtable_set = pathtable_sub.add_parser('set')
+  pathtable_set.set_defaults(func=pathtable.set_path)
+
+  pathtable_set.add_argument("src_ip", help="The origin of the path",
+                              type=str)
+  pathtable_set.add_argument("dst_ip", help="The destination of the path",
+                              type=str)
+  pathtable_set.add_argument("hops", help="The numeric identifiers of the hops that comprise this path.",
+                              nargs="+")
+
+
+def hoptable_args(main_subparser):
+  hoptablep = main_subparser.add_parser('hoptable')
+  hoptable_sub = hoptablep.add_subparsers()
+
+  hoptable_read = hoptable_sub.add_parser('read')
+  hoptable_read.set_defaults(func=hoptable.read)
+
+  hoptable_set = hoptable_sub.add_parser('set')
+  hoptable_set.set_defaults(func=hoptable.set_hops)
+
+  hoptable_set.add_argument('hopid', help="The numeric ID of the hop that you wish to set.",
+                            type=int)
+  hoptable_set.add_argument('bw', help="The speed of this hop in KBps.",
+                            type=int)
+  hoptable_set.add_argument('latency', help="The latency of this hop in milliseconds",
+                            type=int)
 
 def main():
   
@@ -29,6 +65,8 @@ def main():
 
   subp = parser.add_subparsers()
   modelinfo_args(subp)
+  hoptable_args(subp)
+  pathtable_args(subp)
 
   args = parser.parse_args()
   args.func(args)
